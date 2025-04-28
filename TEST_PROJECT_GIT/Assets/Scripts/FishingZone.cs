1c15a348d2e81218de99e32e330eff86d8dc0bc1
aleygv <aleygv@bk.ru>
2025-04-28 23:54:55 +0300

    add prototype of minigame

---
+++ TEST_PROJECT_GIT/Assets/Scripts/FishingZone.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+
+public class FishingZone : MonoBehaviour
+{
+    private SpriteRenderer _sprite;
+    private PlayerMovement _player;
+
+    [SerializeField] private FishingSystem _fishingSystem;
+
+    public event Action OnPlayerInZone;
+
+    private void Start()
+    {
+        _player = FindFirstObjectByType<PlayerMovement>();
+        _sprite = transform.GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _sprite.color = Color.green;
+            OnPlayerInZone?.Invoke();
+            _fishingSystem.StartMiniGame();
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _sprite.color = Color.yellow;
+            
+        }
+    }
+}
