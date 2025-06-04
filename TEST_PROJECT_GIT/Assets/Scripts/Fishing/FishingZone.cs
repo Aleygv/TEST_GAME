ea20caeba7ace8426c16964c35c667439b8fe299
DmitDubr <146427160+DmitDubr@users.noreply.github.com>
2025-06-04 23:37:23 +0300

    final

--- TEST_PROJECT_GIT/Assets/Scripts/Fishing/FishingZone.cs
+++ TEST_PROJECT_GIT/Assets/Scripts/Fishing/FishingZone.cs
@@ -35,33 +35,7 @@ public class FishingZone : MonoBehaviour
             _sprite.color = Color.green;
             OnPlayerInZone?.Invoke();
 
-            // Проверяем, есть ли выбранная наживка
-            Item selectedItem = SelectedItemManager.Instance.selectedItem;
-
-            if (selectedItem != null && selectedItem is BaitItem baitItem)
-            //&& _player.test goes inside if statement(check for minigame input)
-            {
-                // Проверяем, есть ли такая наживка в инвентаре
-                bool hasBait = Inventory.instance.HasItem(baitItem, baitItem.itemType);
-
-                if (hasBait)
-                {
-                    // Запускаем мини-игру
-                    _fishingSystem.StartMiniGame();
-                }
-                else
-                {
-                    Debug.Log("Нет наживки для рыбалки.");
-                    GameManager.ShowNoBaitMessage(); // Можно вызвать UI-оповещение
-                }
-            }
-            else
-            {
-                Debug.Log("Выберите наживку перед рыбалкой.");
-                GameManager.ShowNoBaitMessage(); // Можно вызвать UI-оповещение
-            }
-
-            // Здесь можно отключить движение игрока, если нужно
+            
         }
     }
 
